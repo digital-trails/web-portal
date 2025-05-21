2659d66f67437bb80db6f884ba30ff634906b169
Ibrahim <jsh2hw@virginia.edu>
2025-05-21 14:31:03 -0600

    Update dashboard.component.ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { map, take } from 'rxjs';
 import { UserFacade } from '../store/user/user.facade';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
