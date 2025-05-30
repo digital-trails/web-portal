6dd470f3c6bdd30adcc418fc39bf64823da2dcdd
Ibrahim <jsh2hw@virginia.edu>
2025-05-30 13:06:03 -0600

    destroy

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,30 +1,39 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SafeResourceUrl } from '@angular/platform-browser';
-import { map, switchMap, take } from 'rxjs';
+import { map, Subject, switchMap, take, takeUntil } from 'rxjs';
 import { UserFacade } from '../store/user/user.facade';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
   iframeUrl?: SafeResourceUrl;
+  private destroy$ = new Subject<void>();
 
   constructor(private userFacade: UserFacade, private route: ActivatedRoute) { }
-  
+
   ngOnInit(): void {
     this.route.queryParams.pipe(
-      switchMap(params =>  {
-        return this.userFacade.getUser$().pipe(take(1), map(user => {
-          return user.admin?.studies.get(params["study"]);
-        }));
+      takeUntil(this.destroy$),  // Add takeUntil here
+      switchMap(params => {
+        return this.userFacade.getUser$().pipe(
+          take(1),
+          map(user => user.admin?.studies.get(params["study"]))
+        );
       })
     ).subscribe(url => {
       this.iframeUrl = url;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
