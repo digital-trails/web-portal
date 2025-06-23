6692ec2fb6c8cd58cba77a6ef08bed92fd45b8c4
dt477 <domtr0510@gmail.com>
2025-06-23 13:10:36 -0400

    can get access code, and make post request for github access token (not sure if it works with real access codes yet), but wanted to save somewhere before working on getting scope

--- src/app/builder/auth/auth.component.ts
+++ src/app/builder/auth/auth.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-auth',
@@ -6,4 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent { }
+export class AuthComponent implements OnInit {
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get('code');
+
+    if (code) {
+      console.log('GitHub Code:', code);
+
+      this.http.post<any>(`https://digital-trails.org/api/v1/gh-token?code=${code}`, {})
+        .subscribe({
+          next: (data) => {
+            console.log('Access Token:', data.token);
+            localStorage.setItem('githubAccessToken', data.token);
+            window.location.href = '/builder';
+          },
+          error: (err) => {
+            console.error('Token exchange failed:', err);
+          }
+        });
+    } else {
+      console.error('No code found.');
+    }
+  }
+}
