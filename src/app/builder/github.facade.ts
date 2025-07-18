a14449258bef8cbb0036b11950396bf5741a78bb
dt477 <domtr0510@gmail.com>
2025-07-18 17:19:28 -0400

    can list private repos now

--- src/app/builder/github.facade.ts
+++ src/app/builder/github.facade.ts
@@ -131,7 +131,7 @@ export class GithubFacade {
       Authorization: `bearer ${sessionStorage.getItem('githubAccessToken') || ''}`,
       Accept: 'application/vnd.github.v3+json'
     });
-    return this.http.get<any>(`https://api.github.com/users/${sessionStorage.getItem('githubOwner') || ''}/repos`, { headers });
+    return this.http.get<any>(`https://api.github.com/user/repos`, { headers });
   }
 
   changeOwnerAndRepo(owner: string, repo: string): void {
